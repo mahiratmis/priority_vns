1a1feb88f3fff95cfc9b15041b88c34dc8ddd80d
Mahir Atmis <atmismahir@gmail.com>
2020-02-13 11:19:05 +0300

    some minor styling

--- riskaverse_corrected/benchmark_rules_priority_simopt_riskaverse.py
+++ riskaverse_corrected/benchmark_rules_priority_simopt_riskaverse.py
@@ -1,23 +1,19 @@
-#FCFS, SPT, H rule and HXmu rules analyzed under priority
-#Risk aversion, var_level=5
+# FCFS, SPT, H rule and HXmu rules analyzed under priority
+# Risk aversion, var_level=5
 import json
 import pandas as pd
 import numpy as np
 
-from deap import base
-from deap import creator
-from deap import tools
 
-import simulation_codes  #Andre's package that used in simulation opt.
+import simulation_codes  # Andre's package that used in simulation opt.
 
 
-lamda = 0.5 #test 0, 0.5, 1
+lamda = 0.5  # test 0, 0.5, 1
 var_level = 0.05
 out_fname = "benchmark_rules_vns_db_v{}_l{}_priority_simopt_riskaverse.json".format(int(var_level*100), int(lamda*100))
 input_fname = "results/combined_vns_db_v{}_l{}_priority_simopt_riskaverse.json".format(int(var_level*100), int(lamda*100))
 
 
-###Prune function has to defined earlier
 def optimal_server_number3(priority, FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost):
 
     '''
@@ -25,9 +21,8 @@ def optimal_server_number3(priority, FailureRates, ServiceRates, holding_costs, 
     that number of servers
 
     '''
-    min_nserver = int(sum(np.array(FailureRates)/np.array(ServiceRates)))+1 #min required servers
+    min_nserver = int(sum(np.array(FailureRates)/np.array(ServiceRates))) + 1  # min required servers
     assignment = np.ones((min_nserver, len(FailureRates)))
-
 
     holding_backorder_costList = simulation_codes.SimulationInterface.simulation_optimization_bathrun_priority_riskaverse(lamda,
                                                                                                                           var_level,
@@ -37,56 +32,57 @@ def optimal_server_number3(priority, FailureRates, ServiceRates, holding_costs, 
                                                                                                                           penalty_cost,
                                                                                                                           assignment,
                                                                                                                           priority,
-                                                                                                                          numberWarmingUpRequests = 5000,
-                                                                                                                          stopNrRequests = 100000,
-                                                                                                                          replications = 40)
+                                                                                                                          numberWarmingUpRequests=5000,
+                                                                                                                          stopNrRequests=100000,
+                                                                                                                          replications=40)
 
     server_cost = min_nserver*machineCost
     total_cost = np.mean(holding_backorder_costList)+server_cost
 
     return min_nserver, min_nserver, total_cost, server_cost, total_cost-server_cost
 
-#########Generating different populations###########
-#1-FCFS->
-#2-Shortest and longest proccessing->
-#3-min and max holding cost->
-#4-hxmu long lowest and highest->
+
+# ########Generating different populations###########
+# 1-FCFS->
+# 2-Shortest and longest proccessing->
+# 3-min and max holding cost->
+# 4-hxmu long lowest and highest->
 def population_generator2(failure_rates, service_rates, holding_costs, penalty_cost, skill_cost, machine_cost):
     '''
 
     '''
-    population=[]
-    fcfs_rule=np.ones(len(failure_rates))
+    population = []
+    fcfs_rule = np.ones(len(failure_rates))
     population.append(fcfs_rule)
-    #####service rate#######################SPT
-    ###Fast repairing items have higher priority######
-    priority=np.ones(len(service_rates))
+    # ####service rate#######################SPT
+    # ##Fast repairing items have higher priority######
+    priority = np.ones(len(service_rates))
 
-    k=1
-    for i in np.flip(np.argsort(service_rates),0):
-        priority[i]=k
-        k+=1
+    k = 1
+    for i in np.flip(np.argsort(service_rates), 0):
+        priority[i] = k
+        k += 1
 
     population.append(priority)
 
-    ###holding cost#######################
-    ###Expensive items have higher priortiy##############
-    priority=np.ones(len(holding_costs))
+    # ##holding cost#######################
+    # ##Expensive items have higher priortiy##############
+    priority = np.ones(len(holding_costs))
 
-    k=1
+    k = 1
     for i in np.argsort(holding_costs):
-        priority[i]=k
-        k+=1
+        priority[i] = k
+        k += 1
 
     population.append(priority)
 
-    ###holding *service rate##############
-    priority=np.ones(len(holding_costs))
+    # ##holding *service rate##############
+    priority = np.ones(len(holding_costs))
 
-    k=1
+    k = 1
     for i in np.argsort(np.array(holding_costs)*np.array(service_rates)):
-        priority[i]=k
-        k+=1
+        priority[i] = k
+        k += 1
 
     population.append(priority)
 
@@ -95,39 +91,39 @@ def population_generator2(failure_rates, service_rates, holding_costs, penalty_c
 
 json_case2 = []
 with open(input_fname, "r") as json_file2:
-    #json_file.readline()
+    # json_file.readline()
     for line in json_file2:
         json_case2.append(json.loads(line))
 
 json_case2 = [item for sublist in json_case2[0] for item in sublist]
 
-df=pd.DataFrame.from_dict(json_case2)
+df = pd.DataFrame.from_dict(json_case2)
 
-SPT=[]
-H_rule=[]
-HMU_rule=[]
-FCFS=[]
+SPT = []
+H_rule = []
+HMU_rule = []
+FCFS = []
 
 for i in range(len(df)):
-    if df['utilization_rate'][i]==0.80:
-        FailureRates=np.array(df["FailureRates"][i])
-        ServiceRates=np.array(df["ServiceRates"][i])
-        holding_costs=np.array(df["holding_costs"][i])
-        penalty_cost=df["penalty_cost"][i]
-        skill_cost=df["skillCost"][i]
-        machineCost=df["machineCost"][i]
+    if df['utilization_rate'][i] == 0.80:
+        FailureRates = np.array(df["FailureRates"][i])
+        ServiceRates = np.array(df["ServiceRates"][i])
+        holding_costs = np.array(df["holding_costs"][i])
+        penalty_cost = df["penalty_cost"][i]
+        skill_cost = df["skillCost"][i]
+        machineCost = df["machineCost"][i]
 
-        pop=population_generator2(FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
-        _, _, TotalCost, _, _=optimal_server_number3(pop[0], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
+        pop = population_generator2(FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
+        _, _, TotalCost, _, _ = optimal_server_number3(pop[0], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
         FCFS.append(TotalCost)
 
-        _, _, TotalCost, _, _=optimal_server_number3(pop[1], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
+        _, _, TotalCost, _, _ = optimal_server_number3(pop[1], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
         SPT.append(TotalCost)
 
-        _, _, TotalCost, _, _=optimal_server_number3(pop[2], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
+        _, _, TotalCost, _, _ = optimal_server_number3(pop[2], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
         H_rule.append(TotalCost)
 
-        _, _, TotalCost, _, _=optimal_server_number3(pop[3], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
+        _, _, TotalCost, _, _ = optimal_server_number3(pop[3], FailureRates, ServiceRates, holding_costs, penalty_cost, skill_cost, machineCost)
         HMU_rule.append(TotalCost)
 
     else:
@@ -136,13 +132,13 @@ for i in range(len(df)):
         H_rule.append("NaN")
         HMU_rule.append("NaN")
 
-df["FCFS_cost"]=pd.Series(FCFS)
-df["SPT_cost"]=pd.Series(SPT)
-df["H_rule_cost"]=pd.Series(H_rule)
-df["HMU_rule_cost"]=pd.Series(HMU_rule)
+df["FCFS_cost"] = pd.Series(FCFS)
+df["SPT_cost"] = pd.Series(SPT)
+df["H_rule_cost"] = pd.Series(H_rule)
+df["HMU_rule_cost"] = pd.Series(HMU_rule)
 
-### risk averse parameters
+# ## risk averse parameters
 df["var_level"] = var_level
 df["lamda"] = lamda
 
-df.to_json(out_fname)
+df.to_json(out_fname, orient='records')
--- riskaverse_corrected/results/json_utility.py
+++ riskaverse_corrected/results/json_utility.py
@@ -1,8 +1,9 @@
-import json 
+import json
 import csv
 import pathlib
 
-import pandas as pd 
+import pandas as pd
+
 
 def files_with_extension(path=".", pattern="*.json"):
     """ returns file list that satisfy pattern """
@@ -12,8 +13,8 @@ def files_with_extension(path=".", pattern="*.json"):
         yield file
 
 
-def json_to_csv(out_fname="db_results_combined.csv", 
-                path=".", 
+def json_to_csv(out_fname="db_results_combined.csv",
+                path=".",
                 pattern="*.json"):
     """ convert results given in json format to csv format """
     write_header = True
@@ -26,38 +27,48 @@ def json_to_csv(out_fname="db_results_combined.csv",
                         writer = csv.DictWriter(csvfile, fieldnames=case.keys())
                         if write_header:
                             writer.writeheader()
-                            write_header = False                        
+                            write_header = False
                         # write dictionary to file
                         writer.writerow(case)
 
 
-
-def json_to_csv2(out_fname="db_results_combined.csv", 
-                path=".", 
-                pattern="*.json"):
+def json_to_csv2(out_fname="db_results_combined.csv",
+                 path=".",
+                 pattern="*.json"):
     """ convert results given in json format to csv format """
     for file in files_with_extension(path, pattern):
         with open(file, 'r') as json_file:
             for line in json_file:
                 cases = json.loads(line)
                 cases_df = pd.DataFrame(cases)
-                cases_df.to_csv(out_fname, index=False)                  
+                cases_df.to_csv(out_fname, index=False)
 
 
-def combine_jsons(out_fname="db_results_combined.json", path=".", pattern="*.json"):
+def combine_jsons(out_fname="db_results_combined.json",
+                  path=".", pattern="*.json"):
     # combine json files in current folder
     with open(out_fname, "w") as outfile:
-        outfile.write('[{}]'.format(','.join([open(f, "r").read() for f in files_with_extension(path, pattern)])))
+        outfile.write('[{}]'
+                      .format(','.join([open(f, "r").read()
+                                       for f in files_with_extension(path,
+                                                                     pattern)
+                                        ])
+                              )
+                      )
+
 
 # json to csv
-lamda = 1 #test 0, 0.5, 1
+lamda = 0.5  # test 0, 0.5, 1
 var_level = 0.05
+db_nodb = "db"
+pth = "hybrid"
+dynamic_part = "_{}_v{}_l{}_".format(db_nodb,
+                                     int(var_level*100),
+                                     int(lamda*100))
+sub_pth = "combined_vns" + dynamic_part + "priority_simopt_riskaverse"
 
-#
-db_nodb = "nodb"
-pth = "benchmark"
-o_dir_json = pth + "/combined/json/combined_vns_{}_v{}_l{}_priority_simopt_riskaverse".format(db_nodb, int(var_level*100), int(lamda*100))
-o_dir_csv = pth + "/combined/csv/combined_vns_{}_v{}_l{}_priority_simopt_riskaverse".format(db_nodb, int(var_level*100), int(lamda*100))
-pttern = "*_{}_v{}_l{}*.json".format(db_nodb, int(var_level*100), int(lamda*100))
+o_dir_json = pth + "/combined/json/" + sub_pth
+o_dir_csv = pth + "/combined/csv/" + sub_pth
+pttern = "*" + dynamic_part + "*.json"
 combine_jsons(path=pth, out_fname=o_dir_json+".json", pattern=pttern)
-json_to_csv2(path=pth, out_fname=o_dir_csv+".csv", pattern=pttern)
+json_to_csv(path=pth, out_fname=o_dir_csv+".csv", pattern=pttern)
