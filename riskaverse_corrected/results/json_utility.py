b1a267657ccfdd7761bc21caf9215f3b0be163a0
Mahir Atmis <atmismahir@gmail.com>
2020-06-26 00:01:55 +0300

    varlevel and holding cost runs finished

--- riskaverse_corrected/results/json_utility.py
+++ riskaverse_corrected/results/json_utility.py
@@ -68,12 +68,12 @@ def strip_newlines_in_json(path=".", pattern="*.json"):
 # exit(0)
 
 # json to csv
-lamda = 1 # test 0, 0.5, 1
+lamda = 0.5 # test 0, 0.5, 1
 var_level = 0.05
 db_nodb = "db"
 alg = "vns"
 pth = "sensitivity_extra"
-param ="machine_cost"
+param ="var_level"
 dynamic_part = "{}_{}_v{}_l{}_".format(alg, 
                                      db_nodb,
                                      int(var_level*100),
